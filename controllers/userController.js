ab19be6f1179f44b7582511cacc9d4677e67ed2e
sharikmaslatsuheb123 <sharikmaslatsuheb@gmail.com>
2024-11-19 19:51:04 +0530

    updated code

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,23 @@
-// userController.js
 
-// Add the debug statement before requiring userModel
 // console.log(require.resolve('../models/userModel'));
 
 const User = require('../models/userModel');
 
 // Login handler
+// exports.login = (req, res) => {
+//     const { username, password } = req.body;
+//     User.findByUsername(username, (err, results) => {
+//         if (err) return res.status(500).json({ error: 'Database error' });
+//         if (results.length === 0 || results[0].password !== password) {
+//             return res.status(401).json({ message: 'Invalid credentials' });
+//         }
+//         res.status(200).json({ message: 'Login successful', userId: results[0].id });
+//     });
+// };
+// Updated login controller
 exports.login = (req, res) => {
-    const { username, password } = req.body;
-    User.findByUsername(username, (err, results) => {
+    const { email, password } = req.body;  // Accept email and password
+    User.findByEmail(email, (err, results) => {  // Use the new method to find by email
         if (err) return res.status(500).json({ error: 'Database error' });
         if (results.length === 0 || results[0].password !== password) {
             return res.status(401).json({ message: 'Invalid credentials' });
@@ -17,25 +26,17 @@ exports.login = (req, res) => {
     });
 };
 
-// Signup handler
-// exports.signup = (req, res) => {
-//     const { username, password } = req.body;
-//     User.create(username, password, (err) => {
-//         if (err) return res.status(500).json({ error: 'Database error' });
-//         res.status(201).json({ message: 'User created successfully' });
-//     });
-// };
 exports.signup = (req, res) => {
-    const { username, password } = req.body;
+    const { username, email, password } = req.body;
 
     // Debugging: Log the received data
     console.log('Signup Request Body:', req.body);
 
-    if (!username || !password) {
-        return res.status(400).json({ error: 'Username and password are required' });
+    if (!username || !email || !password) {
+        return res.status(400).json({ error: 'Username, email, and password are required' });
     }
 
-    User.create(username, password, (err) => {
+    User.create(username, email, password, (err) => {
         if (err) {
             console.error('Database error during user creation:', err);
             return res.status(500).json({ error: 'Database error' });
@@ -43,4 +44,3 @@ exports.signup = (req, res) => {
         res.status(201).json({ message: 'User created successfully' });
     });
 };
-
--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,30 @@
+// const db = require('../config/db');
+
+// const User = {
+//     findByUsername: (username, callback) => {
+//         const query = 'SELECT * FROM users WHERE username = ?';
+//         db.query(query, [username], callback);
+//     },
+//     create: (username, password, callback) => {
+//         const query = 'INSERT INTO users (username, password) VALUES (?, ?)';
+//         db.query(query, [username, password], callback);
+//     }
+// };
+
+// module.exports = User;
 const db = require('../config/db');
 
 const User = {
-    findByUsername: (username, callback) => {
-        const query = 'SELECT * FROM users WHERE username = ?';
-        db.query(query, [username], callback);
+    // Find a user by email
+    findByEmail: (email, callback) => {
+        const query = 'SELECT * FROM users WHERE email = ?';
+        db.query(query, [email], callback);
     },
-    create: (username, password, callback) => {
-        const query = 'INSERT INTO users (username, password) VALUES (?, ?)';
-        db.query(query, [username, password], callback);
+
+    // Create a new user with username, email, and password
+    create: (username, email, password, callback) => {
+        const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+        db.query(query, [username, email, password], callback);
     }
 };
 
